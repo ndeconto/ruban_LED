bf3cdf4c9c7e2f962f76aafe295e54088c9f7e01
ndeconto <nicolas.deconto@telecom-paristech.fr>
2017-06-15 23:25:12 +0200

    allume une ed en bleu ? a tester

--- src/main.c
+++ src/main.c
@@ -19,9 +19,20 @@ int main(void)
 	int T = 10;
 
 
-	
+	int i;
+	for (i = 0; i < 4; i++) data[i] = 0;
 
-	wiringPiSPIDataRW(CHANNEL, data, T);
+    //LED1
+    data[5] = 0xff; //on allume a fond, pas de pwm
+    data[6] = 0xff;
+    data[7] = 0;
+    data[8] = 0;
+
+    for (i = 9; i < 13; i++) data[i] = 0xff;
+
+
+
+	wiringPiSPIDataRW(CHANNEL, data, 13);
 
 	//il ne faut pas quitter tout de suite, car des fois ca fout la m*
 	delay(1000);
